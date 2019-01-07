1b19703ea806bc0246595f131b66fa017a3390cd
Alar Aasa <alar@alaraasa.ee>
2019-01-07 20:18:24 +0200

    Simple ClockComponent. Removed SoundMakerComponent empty constructor.

--- classes/ClockComponent.ts
+++ classes/ClockComponent.ts
@@ -1,3 +1,13 @@
 class ClockComponent {
+    public getCurrentHour() {
+        return new Date().getHours();
+    }
 
+    public getCurrentMinute() {
+        return new Date().getMinutes();
+    }
+
+    public getCurrentSecond() {
+        return new Date().getSeconds();
+    }
 }
