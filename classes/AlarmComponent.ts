f35bd35ff784b78e9d557856d889793d90e1c088
Kristjan Liiva <kristjanliiva@gmail.com>
2019-01-07 20:40:06 +0200

    Added simple AlarmComponent

--- classes/AlarmComponent.ts
+++ classes/AlarmComponent.ts
@@ -1,5 +1,27 @@
 class AlarmComponent {
-    constructor(clock: ClockComponent) {
+    private isRepeatable: boolean;
+    private SoundMakerComponent: SoundMakerComponent;
+    private ClockComponent: ClockComponent;
 
+    constructor( clock: ClockComponent, sound: SoundMakerComponent, repeatable: boolean )
+    {
+        this.SoundMakerComponent = sound;
+        this.ClockComponent = clock;
+        this.isRepeatable = repeatable;
+    }
+
+    public setClock( clock: ClockComponent )
+    {
+        this.ClockComponent = clock;
+    }
+
+    public setSoundMaker( soundMaker: SoundMakerComponent )
+    {
+        this.SoundMakerComponent = soundMaker;
+    }
+
+    public setIsRepeatable( isRepeatable: boolean )
+    {
+        this.isRepeatable = isRepeatable;
     }
 }
