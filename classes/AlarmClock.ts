b6989a8e78ca0f6c5fd0db2bc9963cdc2b3cba13
Alar Aasa <alar@alaraasa.ee>
2019-01-08 20:17:24 +0200

    Added IDs to AlarmComponents. Ability to create, edit and delete AlarmComponents.

--- classes/AlarmClock.ts
+++ classes/AlarmClock.ts
@@ -1,3 +1,49 @@
 class AlarmClock {
-    
+    alarms: AlarmComponent[];
+    lastAlarmId: number = 1;
+    timerChecker: number;
+    constructor() {
+        this.timerChecker = setInterval(this.timerCheck, 1000);
+    }
+
+    timerCheck() {
+        this.alarms.forEach( (alarm) => {
+            if (alarm.shouldDelete()) {
+                alarm.getSoundMaker().stopAudio();
+                this.deleteAlarm(alarm.id);
+            } else if (alarm.checkIfShouldPlay() && !alarm.getSoundMaker().isPlaying) {
+                alarm.getSoundMaker().playAudio();
+            } if (!alarm.checkIfShouldPlay() && alarm.getSoundMaker().isPlaying) {
+                alarm.getSoundMaker().stopAudioRepeating();
+            }
+        })
+    }
+
+    deleteAlarm(id: string) {
+        for (let i=0; i < this.alarms.length-1; i++) {
+            if (this.alarms[i].id == id) {
+                this.alarms[i] = null;
+            }
+        }
+        // Should delete the front-end element.
+        document.getElementById(id).outerHTML = "";
+    }
+
+    // Call from front-end - Button to create alarm (+)
+    createNewAlarm() {
+        let newAlarm = new AlarmComponent(this.generateId(), false);
+        this.alarms.push(newAlarm);
+    }
+
+    // Call from front-end - Button to save changes (Apply changes)
+    changeAlarmTime(hour: number, minute: number) {
+        this.alarms.forEach( (alarm) => {
+            alarm.setAlarmTime(hour, minute);
+        })
+    }
+
+    generateId() {
+        this.lastAlarmId++;
+        return "alarm_" + this.lastAlarmId;
+    }
 }
--- classes/AlarmComponent.ts
+++ classes/AlarmComponent.ts
@@ -1,25 +1,44 @@
 class AlarmComponent {
+    public id: string;
     private isRepeatable: boolean;
     private SoundMakerComponent: SoundMakerComponent;
     private ClockComponent: ClockComponent;
-    private alarmTime: string;
+    private alarmHour: number;
+    private alarmMinute: number;
 
-    constructor( clock: ClockComponent, sound: SoundMakerComponent, repeatable: boolean, time: string )
+    constructor( id: string, repeatable: boolean)
     {
-        this.SoundMakerComponent = sound;
-        this.ClockComponent = clock;
+        this.id = id;
+        this.SoundMakerComponent = new SoundMakerComponent();
+        this.ClockComponent = new ClockComponent();
         this.isRepeatable = repeatable;
-        this.alarmTime = time;
+        this.alarmHour = new Date().getHours();
+        this.alarmMinute = new Date().getMinutes()-1;
     }
 
-    public setAlarmTime( time: string )
+    public setAlarmTime( hours: number, minutes: number )
     {
-        this.alarmTime = time;
+        this.alarmHour = hours;
+        this.alarmMinute = minutes;
+    }
+
+    public checkIfShouldPlay() {
+        return this.alarmHour == this.ClockComponent.getCurrentHour() && this.alarmMinute == this.ClockComponent.getCurrentMinute();
+    }
+
+    public shouldDelete() {
+        return ( this.alarmHour != this.ClockComponent.getCurrentHour() || this.alarmMinute != this.ClockComponent.getCurrentMinute() )
+    && this.getSoundMaker().isPlaying && !this.isRepeatable
     }
 
     public setClock( clock: ClockComponent )
     {
         this.ClockComponent = clock;
+    }
+
+    public getSoundMaker()
+    {
+        return this.SoundMakerComponent;
     }
 
     public setSoundMaker( soundMaker: SoundMakerComponent )
@@ -40,9 +59,11 @@ class AlarmComponent {
 
 function renderTemplate( alarm: AlarmComponent )
 {
-    let element = "<div>" + 
+    let element = "<div id=" + this.id + ">" +
     "<input type='time'>" +
     "<input type='checkbox' name='repeatable'>Repeat" +
     "</div>";
-    return document.body.innerHTML = element ;
+    let smc = new SoundMakerComponent();
+    smc.playAudio();
+    return document.body.innerHTML += element ;
 }
